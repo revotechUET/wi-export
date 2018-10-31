96dc33e79d8cbfe8db4d5c9531c3d0f8ec59c2a8
VuThuyThuy97 <vuthuy.uet@gmail.com>
2018-10-31 09:07:33 +0700

    update

--- source/las3/writer.js
+++ source/las3/writer.js
@@ -84,7 +84,7 @@ async function writeDataset(lasFilePath, fileName, project, well, dataset, idCur
     fs.appendFileSync(lasFilePath, space.spaceAfter(19, 'SET') + space.spaceAfter(10, '.') + space.spaceAfter(30, dataset.name) + ':\r\n');
     if (dataset.dataset_params && dataset.dataset_params.length > 0) {
         for (param of dataset.dataset_params) {
-            if(param.mnem != 'SET' && (param.value || param.description ||  param.unit)) {
+            if(param.mnem != 'SET') {
                 let line = space.spaceAfter(19, param.mnem) + space.spaceAfter(10, '.' + param.unit) + space.spaceAfter(30, param.value) + ': ' + param.description + '\r\n';
                 fs.appendFileSync(lasFilePath, line);
             }
