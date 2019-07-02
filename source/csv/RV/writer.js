6a40ff7f40a872cfe19d1077f8812214047f85df
phucnguyenbn9x <phucnguyenbn.9x@gmail.com>
2019-07-02 23:00:22 +0700

    update

--- source/csv/RV/writer.js
+++ source/csv/RV/writer.js
@@ -151,10 +151,16 @@ async function writeCurve(lasFilePath, exportPath, fileName, project, well, data
                 //     tokens = '-9999';
                 // }
 				if (readStreams[i].type == 'NUMBER') {
+                    tokens = [tokens.join(' ')];
 					if (!_.isFinite(parseFloat(tokens[0]))) {
 						tokens = ['-9999'];
 					}
-				} else {
+                } else if (readStreams[i].type == 'TEXT') {
+                    tokens = [tokens.join(' ')];
+                    if (tokens[0].includes('"')) {
+						tokens[0] = tokens[0].replace(/"/g, '');
+                    }
+                } else {
 					tokens = tokens.map((elt, i, arr) => {
 						if (elt.includes('"')) {
 							return elt.replace(/"/g, '');
